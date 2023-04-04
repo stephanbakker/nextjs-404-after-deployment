04a16bf6ae7b9d931f39e68559fce3e4e3d1e822
Stephan Bakker <info@stephanbakker.nl>
2023-04-04 10:58:53 +0200

    add step2 to show that the failure depends on current path

---
+++ src/pages/step2.tsx
@@ -0,0 +1,27 @@
+import { AppProps } from "next/app";
+import { ReactElement } from "react";
+import { Navigation } from "@/components/Navigation";
+import { GetServerSidePropsResult } from "next";
+
+type Props = {
+  content: string
+}
+
+export default function Step1({ content = 'hi!' }: Props): ReactElement {
+  return (
+    <main>
+      <Navigation />
+      <hr />
+      <h1>Step 2</h1>
+      <p>{ content }</p>
+    </main>
+  )
+}
+
+export async function getServerSideProps():Promise<GetServerSidePropsResult<Props>> {
+  return {
+    props: {
+      content: 'Server fetched data here'
+    }
+  }
+}
